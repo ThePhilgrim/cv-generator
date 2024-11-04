8e19ab386720248bb413354e9aa9f61c9c416e14
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2024-11-04 16:32:55 +0100

    Start working on main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
 export default function App() {
-  return <h1 className="text-5xl">Hello</h1>;
+  return (
+    <div className="bg-gray-300 h-dvh px-16 py-8">
+      <div className="grid grid-cols-12 h-full">
+        <div className="col-span-12 md:col-span-5 w-5/6 bg-blue-300 shadow-md px-16 py-8">SIDEBAR</div>
+        <div className="col-span-12 md:col-span-7">CV AREA</div>
+      </div>
+    </div>
+  );
 }
