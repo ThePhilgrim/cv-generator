11b0bc34fd7dcf1c0b33afab96c013b60491cc10
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2024-11-05 16:11:46 +0100

    Change default font style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Lato', 'sans-serif'],
+    },
     colors: {
       gray: {
         50: '#FCFCFD',
