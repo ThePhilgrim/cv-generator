3161eaf7bbe90e9240273ac4bd818fedba39e117
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2024-11-06 08:51:00 +0100

    Create preview of personal profile

--- src/components/CvPreview.jsx
+++ src/components/CvPreview.jsx
@@ -1,10 +1,14 @@
 import GeneralPreview from './GeneralPreview';
+import PersonalProfilePreview from './PersonalProfilePreview';
 
 export default function CvPreview({ generalInfo }) {
   return (
-    <div className="bg-gray-50 shadow-md h-5/6 w-2/3 mx-44">
-      <div className="bg-blue-100 py-8 general-preview">
+    <div className="bg-gray-50 shadow-md h-full w-full mx-32">
+      <div className="bg-blue-100 py-8 general-preview mb-8">
         <GeneralPreview generalInfo={generalInfo} />
+      </div>
+      <div className="personal-profile px-12">
+        <PersonalProfilePreview />
       </div>
     </div>
   );
---
+++ src/components/PersonalProfilePreview.jsx
@@ -0,0 +1,14 @@
+export default function PersonalProfilePreview() {
+  return (
+    <>
+      <div className="bg-blue-100 px-4 mb-4">
+        <span className="text-md">Personal Profile</span>
+      </div>
+      <p className="px-4 text-sm">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime ipsa aspernatur quod, impedit quidem incidunt?
+        Rem, nulla accusantium numquam quaerat dolor voluptas ipsum expedita ad qui, odio quibusdam. Rem, quam! Officia
+        ducimus vitae consectetur ab consequuntur dicta et in earum mollitia. Harum vel dignissimos itaque laborum.
+      </p>
+    </>
+  );
+}
